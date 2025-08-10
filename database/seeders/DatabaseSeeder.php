8cbadb9de4161b8fc103d6f929ae46b0a0f15df0
yazan alkhatib <yazan.alkhatib@almosafer.com>
2025-08-10 11:42:55 +0300

    db operations

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,7 +19,7 @@ class DatabaseSeeder extends Seeder
              UserTableSeeder::class,
              ModelHasRolesTableSeeder::class,
              PermissionTableSeeder::class,
-             RoleHasPermissionsTableSeeder::class,
+//             RoleHasPermissionsTableSeeder::class,
              ModelHasPermissionsTableSeeder::class,
              AppSettingTableSeeder::class,
              MailSeeders::class,
